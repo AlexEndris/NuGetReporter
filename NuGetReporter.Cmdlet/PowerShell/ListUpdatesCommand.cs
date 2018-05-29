4078057a7c38f20a8e2b6b0d5b67250db3276341
AlexEndris <alex@alexendris.com>
2018-05-29 08:10:34 +0200

    Add parameters

--- NuGetReporter.Cmdlet/PowerShell/ListUpdatesCommand.cs
+++ NuGetReporter.Cmdlet/PowerShell/ListUpdatesCommand.cs
@@ -1,13 +1,45 @@
-﻿using System.Management.Automation;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using NuGet.Configuration;
 
 namespace NuGetReporter.Cmdlet.PowerShell
 {
 	[Cmdlet("List", "Updates")]
 	public class ListUpdatesCommand : System.Management.Automation.Cmdlet
 	{
+		[Parameter]
+		public string RootFolder { get; set; } = Environment.CurrentDirectory;
+
+		[Parameter]
+		[Alias("o")]
+		public string Output { get; set; } = $"{Path.Combine(Environment.CurrentDirectory, "nugetreport.json")}";
+		
 		protected override void ProcessRecord()
 		{
+			var sources = GetPackageSources(RootFolder);
+			
+			
+			
 			base.ProcessRecord();
+		}
+
+		private static IEnumerable<PackageSource> GetPackageSources(string directory)
+		{
+			var settings = Settings.LoadDefaultSettings(directory);
+
+			var sourceProvider = new PackageSourceProvider(settings);
+			return sourceProvider.LoadPackageSources();
+		}
+
+		private static IEnumerable<Package> GetPackages(string directory)
+		{
+			Directory.EnumerateFiles(directory, "*.config", SearchOption.AllDirectories)
+				.Union(Directory.EnumerateFiles(directory, "*.csproj", SearchOption.AllDirectories));
+
+			return null;
 		}
 	}
 }
